5b17df201f65915abec26ff9c220ac7a5ab3e3f6
Ale <alemoryta@gmail.com>
2019-08-19 13:55:27 -0300

    Creating Project
    Initial version of the Todoey App

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Alessandro Moryta Suemasu on 19/08/19.
+//  Copyright © 2019 Alessandro Moryta Suemasu. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    // MARK: - Properties
+
+    let itemArray = ["Find Mike", "Buy Eggs", "Destroy Demogorgon"]
+    
+    // MARK: - Lifecycle Events
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // MARK: - TableView DataSource Methods
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+
+    // MARK: - TableView Delegate Methods
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        
+        if cell.accessoryType == .checkmark {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
